bdf495e7325fdb3b66184bfd2883c3cb99ab6984
Pauk21 <niko.paukovic15@gmail.com>
2025-10-18 22:15:00 +0200

    created groups for updateing and drawing

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
-import player as pl
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    player = pl.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(144) / 1000
 
