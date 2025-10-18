395aee2d9dca4f8e12b2aba405b35858888bb7cc
Pauk21 <niko.paukovic15@gmail.com>
2025-10-18 21:48:11 +0200

    added the player

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
+import player as pl
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = pl.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
